eaf21804d9fc01e84d5477c9974e72e6c677796f
Mariusz <m.gogola@lukasiewicz-gorlice.pl>
2026-04-22 12:50:53 +0200

    n6

--- sesje_przyklad/wyloguj.php
+++ sesje_przyklad/wyloguj.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+    <title>Strona prezentująca wykorzystanie sesji</title>
+</head>
+<body>
 <?php
 session_start();
 
@@ -15,3 +25,7 @@ else {
 echo "sesja zniszczpna";
 
 ?>
+
+
+</body>
+</html>

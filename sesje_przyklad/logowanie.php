57e04d9d7efe11c0f5934187cb352b29e17a9f00
Mariusz <m.gogola@lukasiewicz-gorlice.pl>
2026-04-22 11:52:44 +0200

    n3

--- sesje_przyklad/logowanie.php
+++ sesje_przyklad/logowanie.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
 session_start();
 
@@ -24,3 +33,7 @@ else {
         }
 }
 ?>
+    
+</body>
+</html>
+

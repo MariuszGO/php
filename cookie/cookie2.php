213672d39af5e14d58e5ec798d84e28d29f991d2
Mariusz <m.gogola@lukasiewicz-gorlice.pl>
2026-04-24 10:13:19 +0200

    n11

--- cookie/cookie2.php
+++ cookie/cookie2.php
@@ -9,7 +9,24 @@ if(isset($_COOKIE['imie'])){
     setcookie("w", $_COOKIE['w'], time() + 5);
     setcookie("ile",$i,time()+5);
 } else {
-    header("Location: cookie2formularz.php");
+?>
+<form action="cookie20.php" method="get">
+    <label for="imie">Imie: </label>
+    <input type="text" name="imie" id="imie"><br>
+     <label for="nazwisko">Imie: </label>
+    <input type="text" name="nazwisko" id="nazwisko"><br>
+    <lebel for="wiek">Wiek: </label>
+    <select name="wiek" id="wiek">
+    <?php
+    for ($i = 0; $i <= 100; $i++) {
+        echo "<option value='$i'>$i</option>";
+    }
+    ?>
+    </select><br>
+    <button type="submit">Wyślij</button>
+</form> 
+<?php
+
     exit();
 }
 
--- cookie/cookie2formularz.php
+++ cookie/cookie2formularz.php
@@ -1,4 +1,4 @@
-<form action="cookie20.php" method="get">
+<!-- <form action="cookie20.php" method="get">
     <label for="imie">Imie: </label>
     <input type="text" name="imie" id="imie"><br>
      <label for="nazwisko">Imie: </label>
@@ -6,10 +6,10 @@
     <lebel for="wiek">Wiek: </label>
     <select name="wiek" id="wiek">
     <?php
-    for ($i = 0; $i <= 100; $i++) {
-        echo "<option value='$i'>$i</option>";
-    }
+   // for ($i = 0; $i <= 100; $i++) {
+    //    echo "<option value='$i'>$i</option>";
+  //  }
     ?>
     </select><br>
     <button type="submit">Wyślij</button>
-</form>
+</form> -->
